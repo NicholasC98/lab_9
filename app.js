88e424a02881805ae9813459c7501705f5926fb4
Nicholas C <nicholas.chu98@gmail.com>
2021-03-12 17:45:50 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const n1 = 10;
+const n2 = 20;
+let x = 'Peanut';
+let y = true;
+	
+if (n1 === n2) {
+  let a = 'they match';
+  
+  console.log(a)
+} else {
+  let a = 'no luck';
+  
+  console.log(a)
+}
+
+var s = '';
+for (i = 0; i < x.length; i += 1) {
+  s += x[i] + '\n'
+}
+console.log(s)
